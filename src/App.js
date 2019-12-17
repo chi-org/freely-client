0161986b317dd0d937587a504d8c14ebfcbceda1
vivecuervo7 <vivecuervo7@gmail.com>
2019-12-18 08:44:20 +1000

    mocked up activity search form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Header from "./components/common/Header";
 import Activities from "./components/activities/Activities";
 import NewActivity from './components/activities/NewActivity';
+import ActivitySearch from './components/activities/ActivitySearch';
 
 export default () => {
   return (
@@ -12,6 +13,7 @@ export default () => {
       <BrowserRouter>
         <Route exact path="/activities" render={() => <Activities />} />
         <Route exact path="/activities/new" render={() => <NewActivity />} />
+        <Route exact path="/activities/search" render={() => <ActivitySearch />} />
       </BrowserRouter>
     </div>
   );
