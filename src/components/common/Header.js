df46e08bf054a2dd11c6f4d486cbe2b71df65a59
vivecuervo7 <vivecuervo7@gmail.com>
2020-01-04 08:07:23 +1000

    mobile page now has dynamic title

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,19 +3,20 @@ import {MDBNavbar, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, 
 
 export default () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [pageTitle, setPageTitle] = useState("Activities");
 
     return (
         <MDBNavbar light expand="md">
-            <MDBNavbarBrand center className="d-block d-md-none">Activities</MDBNavbarBrand>
+            <MDBNavbarBrand center className="d-block d-md-none">{pageTitle}</MDBNavbarBrand>
             <MDBNavbarToggler onClick={() => setIsOpen(!isOpen)} />
 
             <MDBCollapse isOpen={isOpen} navbar>
                 <MDBNavbarNav left>
                     <MDBNavItem>
-                        <MDBNavLink to="/activities" onClick={() => setIsOpen(false)}>Activities</MDBNavLink>
+                        <MDBNavLink to="/activities" onClick={() => (setIsOpen(false), setPageTitle("Activities"))}>Activities</MDBNavLink>
                     </MDBNavItem>
                     <MDBNavItem>
-                        <MDBNavLink to="/students" onClick={() => setIsOpen(false)}>Student Setup</MDBNavLink>
+                        <MDBNavLink to="/students" onClick={() => (setIsOpen(false), setPageTitle("Student Setup"))}>Student Setup</MDBNavLink>
                     </MDBNavItem>
                 </MDBNavbarNav>
             </MDBCollapse>
