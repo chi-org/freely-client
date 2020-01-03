02bcdf0aaa002428e5bfa26baa56074c92a61d29
vivecuervo7 <vivecuervo7@gmail.com>
2020-01-04 07:28:11 +1000

    added basic navigation between pages

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,26 @@
 import React, {useState} from "react";
-import {MDBNavbar, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavItem} from "mdbreact";
+import {MDBNavbar, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavItem, MDBNavLink} from "mdbreact";
 
 export default () => {
     const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         // <MDBNavbar light expand="md"> use for desktop navbar, commented for testing mobile layout
         <MDBNavbar light>
             <MDBNavbarBrand center>Activites</MDBNavbarBrand>
-            <MDBNavbarToggler onClick={() => {console.log("isOpen:", isOpen); setIsOpen(!isOpen)}} />
+            <MDBNavbarToggler onClick={toggleNavbar} />
 
             <MDBCollapse isOpen={isOpen} navbar>
                 <MDBNavbarNav left>
                     <MDBNavItem active>
-                        <MDBNavbarBrand>Student Setup</MDBNavbarBrand>
+                        <MDBNavLink to="/activities" onClick={toggleNavbar}>Activities</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem active>
+                        <MDBNavLink to="/students" onClick={toggleNavbar}>Student Setup</MDBNavLink>
                     </MDBNavItem>
                 </MDBNavbarNav>
             </MDBCollapse>
