c83941fccd96e852dd19cf5c51521f08e30aee45
Isaac Dedini <vivecuervo7@gmail.com>
2020-01-22 15:58:53 +1000

    test:

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:3030/API/",
-    // baseURL: "https://freely--server.herokuapp.com",
+    // baseURL: "https://freely--server.herokuapp.com/API/",
     withCredentials: true,
     timeout: 4000
 });
