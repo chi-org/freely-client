01736044f7c2ea41425fa57fe0f49f961f056146
carlmccabe <37009889+carlmccabe@users.noreply.github.com>
2020-02-12 15:37:31 +1000

    Connect Server

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3030/API/",
-    // baseURL: "https://freely--server.herokuapp.com/API/",
+//     baseURL: "http://localhost:3030/API/",
+    baseURL: "https://freely--server.herokuapp.com/API/",
     withCredentials: true,
     timeout: 4000
 });
